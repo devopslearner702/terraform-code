5dd7b86f6de89cbfb3db7aa61186eb4cb1374fa3
devopslearner702 <devopslearner702@gmail.com>
2025-03-01 22:26:05 +0530

    updated code

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # terraform.tfvars
 
 region               = "us-east-1"
@@ -12,4 +13,20 @@ public_route_table   = "my-public-rt"
 private_route_table  = "my-private-rt"
 internet_gateway     = "my-igw"
 aws_eip_name         = "my_eip"
+=======
+# terraform.tfvars
+
+region               = "us-east-1"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_cidr   = "10.0.1.0/24"
+private_subnet_cidr  = "10.0.2.0/24"
+availability_zone    = "us-east-1a"
+vpc_name             = "my-custom-vpc"
+public_subnet_name   = "my-public-subnet"
+private_subnet_name  = "my-private-subnet"
+public_route_table   = "my-public-rt"
+private_route_table  = "my-private-rt"
+internet_gateway     = "my-igw"
+aws_eip_name         = "my_eip"
+>>>>>>> fd0bbb365307938f72d1c01f3bff50506609aa7a
 aws_nat_gateway_name  ="my-nat-gtw"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 # variable "aws_access_key_id" {
@@ -84,3 +85,91 @@ variable "aws_eip_name"{
 variable "aws_nat_gateway_name"{
  default=""
 }
+=======
+
+
+# variable "aws_access_key_id" {
+#   description = "AWS Access Key ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "aws_secret_access_key" {
+#   description = "AWS Secret Access Key"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variables.tf
+
+# Define the region for the VPC
+
+variable "region" {
+  description = "The AWS region where the resources will be created"
+  default     = ""
+}
+
+# VPC CIDR block
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = ""
+}
+
+# Public subnet CIDR block
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  default     = ""
+}
+
+# Private subnet CIDR block
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  default     = ""
+}
+
+# Availability Zone for the subnets
+variable "availability_zone" {
+  description = "The availability zone where resources will be created"
+  default     = ""
+}
+
+# VPC name
+variable "vpc_name" {
+  description = "The name of the VPC"
+  default     = ""
+}
+
+# Public subnet name
+variable "public_subnet_name" {
+  description = "The name of the public subnet"
+  default     = "my-public-subnet"
+}
+
+# Private subnet name
+variable "private_subnet_name" {
+  description = "The name of the private subnet"
+  default     = ""
+}
+
+
+variable "public_route_table" {
+  description = "The name of public_route_table"
+  default     = ""
+}
+
+variable "internet_gateway" {
+  description = "The name of internet_gatewaye"
+  default     = ""
+}
+
+variable "private_route_table" {
+  description = "The name of private_route_table"
+  default     = ""
+}
+variable "aws_eip_name"{
+  default =""
+}
+variable "aws_nat_gateway_name"{
+ default=""
+}
+>>>>>>> fd0bbb365307938f72d1c01f3bff50506609aa7a
